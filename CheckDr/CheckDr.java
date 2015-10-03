5af933ca2c892f26c61b0eb0f07801ec1bd7f53e
janchao <janchao@debian>
2015-10-03 12:49:09 +0800

    Create a new project - CheckDr

---
+++ CheckDr/CheckDr.java
@@ -0,0 +1,13 @@
+// Checking Dr. Open
+
+import java.net.URL;
+import java.util.Iterator;
+import org.jsoup.Jsoup;
+
+
+public class CheckDr {
+	public static void main(String[] args) {
+		String drurl = "https://reg.ntuh.gov.tw/WebAdministration/ClinicDt.aspx?x=QQBNAFAATQA9ADEAJgBEAHQAQwBvAGQAZQA9ADYAMAAwADAAMAAyACYAQwBsAGkAbgBpAGMAQwBvAGQAZQA9ADAAMQAmAFMAdQBiAEQAZQBwAHQAQwBvAGQAZQA9ADAAMgAmAEgAbwBzAHAAPQBUADAAJgBEAGUAcAB0AD0ATQBFAEQAJgBEAGEAdABlAD0AMgAwADEANQAvADEAMAAvADAAMgAmAFMAcgB2AEkARAA9ADIAMgAyADgAOAAyADcAJgBUAGUAbQBwAEkARAA9ADIAMgA5ADUA0";
+
+	}
+}

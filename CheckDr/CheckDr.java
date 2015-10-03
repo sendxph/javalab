e080b86f58ff863d598f64d01b8dda8790916115
janchao <janchao@debian>
2015-10-04 01:06:10 +0800

    completed to test some function of jsoup

--- CheckDr/CheckDr.java
+++ CheckDr/CheckDr.java
@@ -3,11 +3,15 @@
 import java.net.URL;
 import java.util.Iterator;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
 
 
 public class CheckDr {
-	public static void main(String[] args) {
-		String drurl = "https://reg.ntuh.gov.tw/WebAdministration/ClinicDt.aspx?x=QQBNAFAATQA9ADEAJgBEAHQAQwBvAGQAZQA9ADYAMAAwADAAMAAyACYAQwBsAGkAbgBpAGMAQwBvAGQAZQA9ADAAMQAmAFMAdQBiAEQAZQBwAHQAQwBvAGQAZQA9ADAAMgAmAEgAbwBzAHAAPQBUADAAJgBEAGUAcAB0AD0ATQBFAEQAJgBEAGEAdABlAD0AMgAwADEANQAvADEAMAAvADAAMgAmAFMAcgB2AEkARAA9ADIAMgAyADgAOAAyADcAJgBUAGUAbQBwAEkARAA9ADIAMgA5ADUA0";
-
+	public static void main(String[] args) throws Exception {
+		URL url = new URL("https://reg.ntuh.gov.tw/WebAdministration/ClinicDt.aspx?x=QQBNAFAATQA9ADEAJgBEAHQAQwBvAGQAZQA9ADYAMAAwADAAMAAyACYAQwBsAGkAbgBpAGMAQwBvAGQAZQA9ADAAMQAmAFMAdQBiAEQAZQBwAHQAQwBvAGQAZQA9ADAAMgAmAEgAbwBzAHAAPQBUADAAJgBEAGUAcAB0AD0ATQBFAEQAJgBEAGEAdABlAD0AMgAwADEANQAvADEAMAAvADAAMgAmAFMAcgB2AEkARAA9ADIAMgAyADgAOAAyADcAJgBUAGUAbQBwAEkARAA9ADIAMgA5ADUA0");
+		Document doc = Jsoup.parse(url, 3000);
+		
 	}
 }

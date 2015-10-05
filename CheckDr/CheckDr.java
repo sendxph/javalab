c3a6cba74c53e6628fa1e4c9d8d9bea300f37463
janchao <janchao@d1908.enterprisenet.org>
2015-10-05 17:48:15 +0800

    rename some files

--- CheckDr/CheckDr.java
+++ CheckDr/CheckDr.java
@@ -10,9 +10,12 @@ import org.jsoup.select.Elements;
 
 public class CheckDr {
 	public static void main(String[] args) throws Exception {
-		URL url = new URL("https://reg.ntuh.gov.tw/WebAdministration/DtQueryB.aspx?Name=%E7%8E%8B%E6%B2%BB%E5%85%83");
+		String dr_url = "https://reg.ntuh.gov.tw/WebAdministration/DtQueryB.aspx?Name=%E7%8E%8B%E6%B2%BB%E5%85%83";
+		int retry = 10;
+		URL url = new URL(dr_url);
 		Document doc = Jsoup.parse(url, 3000);
 		Elements sta = doc.select("td");
+		//System.out.println(sta.get(100).text());
 		System.out.println(sta.get(19).text());
 		if ( (sta.get(19).text()).equals("停止掛號") ) {
 			System.out.println("No......無法掛號");

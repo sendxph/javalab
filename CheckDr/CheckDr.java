f6ce4415c57cc5c1f2ac3b4fd85d231b749bc192
janchao <janchao@debian>
2015-10-06 22:45:24 +0800

    CheckDr: add while loop and change checking function

--- CheckDr/CheckDr.java
+++ CheckDr/CheckDr.java
@@ -11,18 +11,31 @@ import org.jsoup.select.Elements;
 public class CheckDr {
 	public static void main(String[] args) throws Exception {
 		String dr_url = "https://reg.ntuh.gov.tw/WebAdministration/DtQueryB.aspx?Name=%E7%8E%8B%E6%B2%BB%E5%85%83";		// Set Dr. url
-		int retry = 10;		// Set retry time
-		String sta = "";	// Dr. status
+		int n = 1;			// Check number
+		int retry = 5000;	// retry time
+		String sta = "";		// Dr. status
 		
-		URL url = new URL(dr_url);
-		Document doc = Jsoup.parse(url, 3000);
-		Elements td = doc.select("td");
-		//System.out.println(td.get(100).text());
-		System.out.println(td.get(19).text());
-		if ( (td.get(19).text()).equals("停止掛號") ) {
-			System.out.println("No......無法掛號");
-		} else {
-			System.out.println("Yes.....可以掛號了!!");
+		while (sta.equals("")) {
+			try {
+				Thread.sleep(retry);
+				URL url = new URL(dr_url);
+				Document doc = Jsoup.parse(url, 3000);
+				
+				Elements td = doc.select("td");
+				sta = td.get(19).text();
+				System.out.println(String.format("第 %d 次檢查:", n));
+				System.out.println(">> " + sta);
+				if ( sta.equals(". 掛號 .") ) {
+					n += 1;
+					System.out.println(">> ^^可以掛號了^^");
+				} else {
+					n += 1;
+					System.out.println(">> !!仍不能掛號!!");
+					sta = "";
+				}
+			} catch (Exception e) {
+				System.out.println("Has an exception");
+			}	
 		}
 	}
 }

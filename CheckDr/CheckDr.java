39eac3a05afd25a4dde5ebd1869044c4a101fe80
janchao <janchao@d1908.enterprisenet.org>
2015-10-06 21:31:22 +0800

    CheckDr.java: update global varialbe

--- CheckDr/CheckDr.java
+++ CheckDr/CheckDr.java
@@ -1,4 +1,4 @@
-// Checking Dr. Open
+// Checking Dr.
 
 import java.net.URL;
 import java.util.Iterator;
@@ -10,15 +10,16 @@ import org.jsoup.select.Elements;
 
 public class CheckDr {
 	public static void main(String[] args) throws Exception {
-		String dr_url = "https://reg.ntuh.gov.tw/WebAdministration/DtQueryB.aspx?Name=%E7%8E%8B%E6%B2%BB%E5%85%83";
-		int retry = 10;
+		String dr_url = "https://reg.ntuh.gov.tw/WebAdministration/DtQueryB.aspx?Name=%E7%8E%8B%E6%B2%BB%E5%85%83";		// Set Dr. url
+		int retry = 10;		// Set retry time
+		String sta = "";	// Dr. status
 		
 		URL url = new URL(dr_url);
 		Document doc = Jsoup.parse(url, 3000);
-		Elements sta = doc.select("td");
-		//System.out.println(sta.get(100).text());
-		System.out.println(sta.get(19).text());
-		if ( (sta.get(19).text()).equals("停止掛號") ) {
+		Elements td = doc.select("td");
+		//System.out.println(td.get(100).text());
+		System.out.println(td.get(19).text());
+		if ( (td.get(19).text()).equals("停止掛號") ) {
 			System.out.println("No......無法掛號");
 		} else {
 			System.out.println("Yes.....可以掛號了!!");

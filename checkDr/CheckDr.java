4df9c22f0ca28a811eba95e7c493ecc9759be6b6
janchao <janchao@debian>
2015-10-11 22:10:27 +0800

    update: loop to check all td is completed

--- checkDr/CheckDr.java
+++ checkDr/CheckDr.java
@@ -11,16 +11,16 @@ public class CheckDr {
 	public static void main(String[] args) throws Exception {
 		//String dr_url = "https://reg.ntuh.gov.tw/WebAdministration/DtQueryB.aspx?Name=%E7%8E%8B%E6%B2%BB%E5%85%83";		// real Dr url
 		String dr_url = "https://reg.ntuh.gov.tw/WebAdministration/DtQueryA.aspx?x=SABvAHMAcAA9AFQAMAAmAE4AYQBtAGUAPQAhfE56wHk1";	// test Dr url
-		int n = 1;			// Check number
+		int n = 1;			// the number of checking
 		int retry = 2000;	// retry time
 		String sta = "";		// Dr. status
+		int hasFree = 0;		// 有空缺可以掛號的數量
 		
 		while (sta.equals("")) {
 			try {
 				Thread.sleep(retry);
 				Document doc = Jsoup.connect(dr_url).get();
-				
-				Elements td = doc.select("td");
+				/* Elements td = doc.select("td");
 				sta = td.get(20).text();
 				System.out.println(String.format("第 %d 次檢查:", n));
 				System.out.println(">> " + sta);
@@ -31,7 +31,28 @@ public class CheckDr {
 					n += 1;
 					System.out.println(">> !!仍不能掛號!!");
 					sta = "";
+				} */
+
+				System.out.printf("第 %d 次檢查: ", n);
+				Elements td = doc.select("td");
+				System.out.printf("有 %d 個td\n", td.size());
+				for (int i=0; i < td.size(); i++) {
+					sta = td.get(i).text();
+					if ( sta.equals(". 掛號 .") ) {
+						hasFree += 1;
+					}
 				}
+
+				if ( hasFree == 0 ) {
+					System.out.println(">> XXX 仍然無法掛號！ XXX");
+				} else {
+					System.out.printf(">> 有 %d 個空缺，請趕快掛號！\n", hasFree);
+					// 發送 mail 放在此
+				}
+				n += 1;
+				sta = "";
+				hasFree = 0;
+				System.out.println();
 			} catch (Exception e) {
 				System.out.println("Has an exception");
 			}	

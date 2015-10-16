9ded6d17872e3f7436e3c40952d9110f3c84d56c
janchao <janchao@debian>
2015-10-17 00:10:12 +0800

    change some code to better: equales...etc

--- checkDr/CheckDr.java
+++ checkDr/CheckDr.java
@@ -14,18 +14,18 @@ public class CheckDr {
 		int retry = 1000;	// retry interval time
 		String sta = "";		// Dr. status
 		int hasFree = 0;		// 有多少空缺數可以掛號
-		
+
 		while (sta.equals("")) {
 			try {
 				Thread.sleep(retry);
+								
 				Document doc = Jsoup.connect(dr_url).get();
-
-				System.out.printf("第 %d 次檢查: ", n);
 				Elements td = doc.select("td");
+				System.out.printf("第 %d 次檢查: ", n);
 				System.out.printf("有 %d 個td\n", td.size());
 				for (int i=0; i < td.size(); i++) {
 					sta = td.get(i).text();
-					if ( sta.equals(". 掛號 .") ) {
+					if ( ". 掛號 .".equals(sta) ) {
 						hasFree += 1;
 					}
 				}
@@ -36,6 +36,7 @@ public class CheckDr {
 					System.out.printf(">> %d 個時段有空缺，請趕快掛號！\n", hasFree);
 					// 發送 mail 放在此
 				}
+
 				n += 1;
 				sta = "";
 				hasFree = 0;

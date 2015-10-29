5c4cef5149a9385a131271375059d07d033cbe2e
janchao <janchao@debian>
2015-10-29 23:54:48 +0800

    learning: number function test in Demo1.java

--- learning/Demo1.java
+++ learning/Demo1.java
@@ -1,6 +1,15 @@
 class Demo1 {
     public static void main(String[] args) {
         int num = 123;
-        System.out.println("測試加入數字：" + num);
+        System.out.println("Origin number is " + num);
+
+        num += 2;
+        System.out.println("+=2 is " + num);
+
+        num <<= 2;
+        System.out.println("<<=2 is " + num);
+
+        num >>= 2;
+        System.out.println(">>=2 is " + num);
     }
 }
